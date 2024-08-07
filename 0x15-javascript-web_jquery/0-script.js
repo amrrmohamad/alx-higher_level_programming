744eacc9e74a325c09f0312d608be39fead69024
amrrmohamad <amr.r.mohamad55@gmail.com>
2024-08-07 17:17:52 +0300

    alx website

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,3 @@
+// Update text color to Red #FF0000
+
+document.querySelector('head').style.color = '#FF0000';
---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,3 @@
+// Use jQuery APIs to red #FF0000
+
+$('header').css('color', '#FF0000');
