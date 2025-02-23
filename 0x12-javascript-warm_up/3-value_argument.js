22adfd0fbb7d00532bf5477e6df9cbc3b55cfc7e
amrrmohamad <amr.r.mohamad55@gmail.com>
2025-02-23 11:32:05 +0200

    alx

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -4,10 +4,8 @@ const process = require('process');
 
 const args = process.argv.slice(2);
 
-if (args.length == 0) {
+if (args[0] == undefined) {
 	console.log("No argument");
 } else {
-	for (let i = 0; i < args.length; i++) {
-		console.log(args[i]);
-	}
+	console.log(args[0]);
 }
