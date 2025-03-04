635da41f093ae0a2e25e799ca976c3d2035d1663
amrrmohamad <amr.r.mohamad55@gmail.com>
2025-03-05 00:28:03 +0200

    alx

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const arg = require('process');
+const num = process.argv[2];
+
+if (!isNaN(num)){
+	for(let i = 0; i < num ; i++){
+		let row = '';
+		for(let j = 0; j < num; j++){
+			row += 'X';
+		}
+		console.log(row);
+	}
+}else{
+	console.log('Missing size');
+}
+
