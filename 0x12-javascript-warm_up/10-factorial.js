74f7e964471dd83ea53218fdd79eb7e3f4a64541
amrrmohamad <amr.r.mohamad55@gmail.com>
2025-03-05 03:43:12 +0200

    alx

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,9 +2,15 @@
 
 const arg = require('process');
 const num = arg.argv[2];
-let res = 1;
 
-for (let i = num; i > 1; i-- ){
-	res = res * i
+let res = 1;
+function factorial(n){
+	while(n != 0 && !isNaN(n)){
+		res = res * n;
+		n--;
+		return factorial(n);
+	}
+	return res;
 }
-console.log(res);
+
+console.log(factorial(num));
